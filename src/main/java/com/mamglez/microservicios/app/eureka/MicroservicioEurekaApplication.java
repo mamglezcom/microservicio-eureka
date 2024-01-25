09f4cd752ef953775a253a528b500d2c092849d9
DEVELOPER\alqui <contacto@mamglez.com>
2024-01-25 19:59:03 +0100

    enable eureka server

--- src/main/java/com/mamglez/microservicios/app/eureka/MicroservicioEurekaApplication.java
+++ src/main/java/com/mamglez/microservicios/app/eureka/MicroservicioEurekaApplication.java
@@ -2,8 +2,10 @@ package com.mamglez.microservicios.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroservicioEurekaApplication {
 
 	public static void main(String[] args) {
